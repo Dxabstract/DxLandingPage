65ecb2ef30099bfb7a0ee4170e81d09558670002
Jhoan Raigoza <raigoxd@LaR-PC.local>
2024-08-06 11:11:57 -0500

    Deploy Changes

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,15 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { LanguageProvider } from './context/languageContext.jsx'
+import { HashRouter } from 'react-router-dom';
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <LanguageProvider>
-      <App />
-    </LanguageProvider>
+    <HashRouter>
+      <LanguageProvider>
+        <App />
+      </LanguageProvider>
+    </HashRouter>
   </React.StrictMode>,
 )
